67c4c227a9aacaca67d443e86038110441f7cb60
codetector <codetector@codetector.cn>
2019-07-10 21:11:28 +0800

    no compile

--- llvm/lib/Target/LC3/LC3TargetMachine.cpp
+++ llvm/lib/Target/LC3/LC3TargetMachine.cpp
@@ -3,8 +3,16 @@
 //
 
 #include "LC3TargetMachine.h"
+#include "LC3.h"
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/CodeGen/TargetLoweringObjectFileImpl.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Support/TargetRegistry.h"
 
-llvm::LC3TargetMachine::LC3TargetMachine(const llvm::Target &t, llvm::StringRef dataLayoutString,
+using namespace llvm;
+
+LC3TargetMachine::LC3TargetMachine(const llvm::Target &t, llvm::StringRef dataLayoutString,
                                          const llvm::Triple &targetTriple, llvm::StringRef cpu, llvm::StringRef fs,
                                          const llvm::TargetOptions &options, const llvm::Target &T,
                                          const llvm::Triple &TT, llvm::StringRef CPU, llvm::StringRef FS,
@@ -12,4 +20,8 @@ llvm::LC3TargetMachine::LC3TargetMachine(const llvm::Target &t, llvm::StringRef 
                                          llvm::CodeModel::Model CM, llvm::CodeGenOpt::Level OL) : TargetMachine(t, dataLayoutString, targetTriple, cpu, fs, options) {
 }
 
+
+extern "C" void LLVMInitializeLC3Target() {
+    RegisterTargetMachine<LC3TargetMachine> X(TheLC3Target);
+}
 
