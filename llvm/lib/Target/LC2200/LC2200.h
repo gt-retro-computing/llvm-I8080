8ca37731b06f4c907b7b27ed2cba7d96fd2f8765
Codetector <codetector@codetector.cn>
2019-08-29 18:11:30 -0400

    partial 1st stage [up till InstrInfo]

---
+++ llvm/lib/Target/LC2200/LC2200.h
@@ -0,0 +1,17 @@
+//
+// Created by codetector on 8/29/19.
+//
+
+#ifndef TARGET_LC2200_H
+#define TARGET_LC2200_H
+#include "MCTargetDesc/LC2200MCTargetDesc.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm {
+    class TargetMachine;
+    class LC2200TargetMachine;
+    FunctionPass *createLC2200ISelDag(LC3TargetMachine &TM, CodeGenOpt::Level OptLevel);
+} // end namespace llvm;
+
+#endif
+
